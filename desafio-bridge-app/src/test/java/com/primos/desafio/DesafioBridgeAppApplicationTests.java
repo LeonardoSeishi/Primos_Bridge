cee1af7e32e43fab8a22d359ab78c771b57f0fa1
Leonardo <leonardosyamazaki@gmail.com>
2024-04-22 03:29:41 -0300

    spring boot initializr

---
+++ desafio-bridge-app/src/test/java/com/primos/desafio/DesafioBridgeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primos.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBridgeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
